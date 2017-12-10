bbb34d60a7ad6183752cc98bbe56651508a26929
Henrique Vasconcelos <hmullermail@gmail.com>
2017-12-09 21:21:36 -0300

    Now it should work

--- gpio_example.py
+++ gpio_example.py
@@ -6,27 +6,45 @@ import time
 
 #logging.basicConfig(format='%(levelname)s-%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG,filename='/App/gpio.log')
 
+mpin = 14
+tpin = 15
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(mpin, GPIO.IN)
+GPIO.setup(tpin, GPIO.OUT)
+
+cap = 0.000001
+adj = 2.130620985
+
+inter = 0
+adder = 0
+nopts = 10
+
 # Set GPIO mode: GPIO.BCM or GPIO.BOARD
-GPIO.setmode(GPIO.BOARD)
-
-# GPIO pins list based on GPIO.BOARD
-gpioList1 = [3,5,7,8,19,11,12,13,15]
-gpioList2 = [16,18,10,21,22,23,24,26]
-
-# Set mode for each gpio pin
-GPIO.setup(gpioList1, GPIO.OUT)
-GPIO.setup(gpioList2, GPIO.OUT)
 
 while True:
-	# Change gpio pins in list 1 from low to high and list 2 from high to low
-	GPIO.output(gpioList1, 1)
-	GPIO.output(gpioList2, 0)
-	time.sleep(1)
 
-	# Change gpio pin in list 1 from high to low and list 2 from low to high
-	GPIO.output(gpioList1, 0)
-	GPIO.output(gpioList2, 1)
-	time.sleep(1)
+  lapsetime_1=time.time()
+  while inter<nopts:
+      GPIO.output(tpin, False)
+      time.sleep(0.2)
+      GPIO.output(tpin, True)
+      starttime=time.time()
+      endtime=time.time()
+      while (GPIO.input(mpin) == GPIO.LOW):
+          endtime=time.time()
+      measureresistance=endtime-starttime
+      res=(measureresistance/cap)*adj
+      inter++
+      adder=adder+res
+  
+  lapsetime_2=time.time()
+  timelapse=lapsetime_2-lapsetime_1
+  
+  println(adder/nopts)
+  println('For '+nopts+' measurements, darkness sensor reading is '+adder/nopts+'.')
+  println('Total elapsed time: '+timelapse+' seconds.')
 
 # Reset all gpio pin
 GPIO.cleanup()
